049a6fdf1ee85c22658313c35a5abd441945f452
Vanja Oljaca <vanja.oljaca@me.com>
2023-07-14 23:01:51 -1000

    Got module sharing working finally

--- metro.config.js
+++ metro.config.js
@@ -5,18 +5,24 @@
  * @format
  */
 
-const {getDefaultConfig} = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const path = require('path');
+const extraNodeModules = {
+    'vanjacloud.shared.js': path.resolve(__dirname + '/../vanjacloud.shared.js'),
+};
 
 const config = getDefaultConfig(__dirname);
 
 config.transformer.getTransformOptions = async () => ({
     transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true
+        experimentalImportSupport: false, inlineRequires: true
     }
 });
 config.resolver.extraNodeModules = {
-    "vanjacloudjs.shared": "lib"
+    'vanjacloud.shared.js': path.resolve(__dirname + '/../vanjacloud.shared.js')
 };
+
+config.watchFolders = [path.resolve(__dirname + '/../vanjacloud.shared.js')];
 
 module.exports = config;
